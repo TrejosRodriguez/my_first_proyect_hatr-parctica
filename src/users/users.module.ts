f28452323dd60cbd4f915f10992170ee5674f850
TrejosTrejos <trejosrodriguez91@outlook.es>
2025-10-08 21:46:06 -0600

    Actualizacion de la guia numero 2

---
+++ src/users/users.module.ts
@@ -0,0 +1,13 @@
+// src/users/users.module.ts
+
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { PrismaModule } from '../prisma/prisma.module'; // Importa el módulo de Prisma
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [PrismaModule], // Hace que PrismaService esté disponible
+})
+export class UsersModule {}
